7f506fbb0b5631059b28f944ab0843fef71b3e05
Rafhael <rafhael.brum@gmail.com>
2025-04-23 08:52:42 -0300

    app/server basic setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("server running");
+import app from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT; // 3000 from .env
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${ PORT }`);
+});
